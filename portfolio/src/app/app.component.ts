63b2f34f2c8897af3cf1aed5a788f4384c1572cd
AlkanTetik <alkan.tetik.at@gmail.com>
2025-05-10 19:31:26 +0200

    converted portfolio folder from submodule to regular folder

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
+import { HeaderComponent } from "./header/header.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'portfolio';
+}
