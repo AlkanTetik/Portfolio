be8f69f84216fd49f8f737ec9c85f2ccc8100f4f
AlkanTetik <alkan.tetik.at@gmail.com>
2025-05-14 17:18:26 +0200

    Added an image of myself in about me section and completed. Also added the portfolio component to the project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component
 import { HeaderComponent } from "./header/header.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent, MySkillsComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
