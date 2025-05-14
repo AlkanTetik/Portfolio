c46dde7c8d801bbd89ae08f8bead957762e232c1
AlkanTetik <alkan.tetik.at@gmail.com>
2025-05-14 15:42:30 +0200

    continued with about me section. started with the section my skills and completed. Sectionportfolio loading..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { AboveTheFoldComponent } from "./above-the-fold/above-the-fold.component";
 import { HeaderComponent } from "./header/header.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent],
+  imports: [RouterOutlet, AboveTheFoldComponent, HeaderComponent, AboutMeComponent, MySkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
