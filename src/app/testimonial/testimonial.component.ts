61203dde88f250f70e242a83a04233635ed950ea
AlkanTetik <alkan.tetik.at@gmail.com>
2025-05-17 17:22:21 +0200

    Continued with the Testimonial section. Added some pictures of the persons in testimonial.Changed some styling.

--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -13,19 +13,19 @@ export class TestimonialComponent {
       quote: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
       name: "V. Schuster",
       role: "Team Partner",
-      image: "assets/self.png"  // Bildpfad anpassen
+      image: "assets/testimonial2.jpg"  // Bildpfad anpassen
     },
     {
       quote: "Robert always brought fresh ideas to the table and helped us overcome tough challenges. His creativity really made a difference in the project.",
       name: "Robert",
       role: "Design Lead",
-      image: "assets/self.png" // Bildpfad anpassen
+      image: "assets/testimonial3.jpg" // Bildpfad anpassen
     },
     {
       quote: "Hasan was incredibly reliable and technically strong. He solved issues before they even became problems. A true asset to the team.",
       name: "Hasan",
       role: "Frontend Developer",
-      image: "assets/self.png" // Bildpfad anpassen
+      image: "assets/testimonial1.png" // Bildpfad anpassen
     }
   ];
 
